ce6e3acbbf7808785f013100f0bfe8364a0c7c08
“Marcos <marcos.gaitan.1704@gmail.com>
2022-05-09 18:55:18 -0300

    add Building [entities]

---
+++ src/main/java/app/entities/Building.java
@@ -0,0 +1,31 @@
+package app.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+
+@Entity // persistence
+@Data // Lombok = auto-boilerplate
+@Table(name = "building") // database
+public class Building implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "buildingName", columnDefinition = "varchar(45)")
+	@NotEmpty()
+	private String buildingName; // no ingresar nombres en los atributos que sean iguales al nombre de la misma clase
+									 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idBuilding;
+
+}
