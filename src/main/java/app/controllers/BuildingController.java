7b745f6db4f91e0baed75940ec0c0714a043ad70
“Marcos <marcos.gaitan.1704@gmail.com>
2022-05-09 18:58:01 -0300

    add Building controller and view

---
+++ src/main/java/app/controllers/BuildingController.java
@@ -0,0 +1,87 @@
+package app.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import app.entities.Building; 
+import app.services.implementation.BuildingService;
+import lombok.var;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/building") // Building route
+@Slf4j
+public class BuildingController {
+	@Autowired(required=true)
+	private BuildingService buildingService;
+	
+	@GetMapping("/buildings")
+	public String Buildings(Model model)
+	{
+		log.info("CONTROLLER [Building]"); 	// info console: Para no perder la pista del controlador (opcional)
+		log.debug("METHOD [Buildings]");	// details console: Para saber que metodo se esta ejecutando (opcional)
+		
+		var listBuilding = buildingService.getAll(); // var = Lombok		
+		
+		// inyeccion Thymeleaf
+		model.addAttribute("listBuilding", listBuilding);
+		
+		return "building/listBuilding";
+	}
+	
+	@GetMapping("/addBuilding")
+	public String addBuilding(Building building, Model model)
+	{		
+		log.info("CONTROLLER [Building]"); // info console
+		log.debug("METHOD [addBuilding]"); // details console
+ 
+		return "building/insertOrUpdate"; // go to: pagina de insertar o modificar (Building)
+	}
+
+	@PostMapping("/addBuilding")
+	public String saveBuilding(@Valid Building building, Errors error) // Inyecta automaticamente al ser metodo <post> busca en: th:action="@{/addBuilding}" method="post"
+	{		
+		log.info("CONTROLLER [Building]"); 	// info console
+		log.debug("METHOD [saveBuilding]");	// details console
+		
+		if(error.hasErrors()) // En caso de un error en las validaciones
+		{
+			return "building/insertOrUpdate"; // Se queda en la pagina y muestra los errores
+		}
+		 
+		buildingService.insertOrUpdate(building); // En caso de que funcione agrega el rol
+		
+		return "redirect:/building/buildings"; // go to: home page
+	}
+	
+	// Type: Path variable
+	@GetMapping("/edit/{idBuilding}") // Al pasarle el parametro {idBuilding} lo relaciona con el parametro de Building
+	public String editBuilding(Building building, Model model)
+	{		
+		log.info("CONTROLLER [Building]");	// info console
+		log.debug("METHOD [editBuilding]");	// details console
+		
+		model.addAttribute("building", buildingService.findById(building.getIdBuilding())); // Necesario "instanciar" el objeto para ser mostrado en Thymeleaf
+	 
+		return "building/insertOrUpdate"; // go to: pagina de insertar o modificar (role)
+	}
+	
+	// Type: Query Parameter
+	@GetMapping("/delete") // Relaciona el IdRole en el HTML con el controlador para "apuntar" al correcto
+	public String deleteBuilding(Building building)
+	{		
+		log.info("CONTROLLER [Building]");		// info console
+		log.debug("METHOD [deleteBuilding]");	// details console
+		
+		buildingService.remove(building.getIdBuilding()); // remueve el rol
+		
+		return "redirect:/building/buildings"; // go to: home page
+	}
+}
