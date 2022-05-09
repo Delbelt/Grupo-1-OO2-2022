6126a6cc67930d5de9f7ff6f25c55ec13757311c
Charruax <matbas99x@gmail.com>
2022-05-09 15:37:37 -0300

    add Classroom Service and Implementation(services)

---
+++ src/main/java/app/services/IClassroomService.java
@@ -0,0 +1,19 @@
+package app.services;
+
+import java.util.List;
+
+import app.entities.Classroom;
+
+public interface IClassroomService {
+	
+	// custom methods for testings
+	
+	public Classroom findById(int idClassroom); // traer aula por id
+	
+	public List<Classroom> getAll(); // traer la lista de todas las aulas
+	
+	public boolean insertOrUpdate(Classroom classroom); // agrega o modifica un aula
+	
+	public boolean remove(int idClassroom); // elimina un aula
+
+}
---
+++ src/main/java/app/services/implementation/ClassroomService.java
@@ -0,0 +1,52 @@
+package app.services.implementation;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import app.entities.Classroom;
+import app.repositories.IClassroomRepository;
+import app.services.IClassroomService;
+
+@Service
+public class ClassroomService implements IClassroomService {
+	
+	@Autowired() // injection
+	private IClassroomRepository repository;
+	
+	@Override
+	@Transactional(readOnly = true) // no modifica la bd, solamente lectura
+	public Classroom findById(int idClassroom)
+	{
+		return repository.findById(idClassroom);
+	}
+
+	@Override
+	@Transactional(readOnly = true) // no modifica la bd, solamente lectura
+	public List<Classroom> getAll() {
+		return repository.findAll();
+	}
+
+	@Override
+	@Transactional // modifica la BD: commit / rollback
+	public boolean insertOrUpdate(Classroom classroom) {
+		return repository.save(classroom) != null ? true : false;
+	}
+
+	@Override
+	@Transactional // modifica la BD: commit / rollback
+	public boolean remove(int idClassroom) {
+		
+		try
+		{
+			repository.deleteById(idClassroom); return true;			
+		} 
+		
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+}
